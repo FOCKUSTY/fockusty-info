a82dae4e761d5b458ca29af47cf1b5698d9e76ea
FOCKUSTY <wwddwwwd@mail.ru>
2024-10-16 16:33:38 +0500

    updated dropdown component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,22 @@
+import styles from "./app.module.css";
+
 import React from "react";
 
 import Layout from "../ui/layout.ui";
 import Projects from "../ui/components/projects.components";
 
-import styles from "./app.module.css";
+import Dropdown from "../ui/components/dropdown.component";
 
 class Page extends Layout {
 	private readonly Page = () => {
 		return (
 			<div className={`page ${styles.page}`}>
 				<main id={styles.main}>
+					<Dropdown content={[
+						<div>Hello!</div>,
+						<div>Hello!</div>,
+						<div>Hello!</div>
+						]} name="test"/>
 					<div id={styles.projects}>
 						<h2 id={`${styles.projects}_h2`}>Мои проеты</h2>
 						<Projects />
--- src/ui/components/dropdown.component.tsx
+++ src/ui/components/dropdown.component.tsx
@@ -5,6 +5,7 @@ import React from "react";
 type Props = {
     name: string
     content: React.ReactElement[];
+    styles?: {[key: string]: string}
 };
 
 class Component extends React.Component<Props> {
@@ -13,16 +14,24 @@ class Component extends React.Component<Props> {
     };
 
     private Component() {
+        const getStyles = (key: string) => {
+            const componentStyles = `${styles[key]} ${this.props.styles ? this.props.styles[key] : ''}`;
+            
+            return componentStyles;
+        };
+
         return (
-            <div id={`container_${this.props.name}`}>
+            <div id={`${styles.dropdown}_${this.props.name}`} className={getStyles('dropdown')}>
                 <div>
-                    <button id={`${styles.dropbtn}_${this.props.name}`} className={styles.dropbtn}>
+                    <button id={`${styles.dropbtn}_${this.props.name}`} className={getStyles('dropbtn')}>
                         <span id={this.props.name}>{this.props.name}</span>
                     </button>
                 </div>
 
-                <div id={`${styles.dropdown_content}_${this.props.name}`} className={styles.dropdown_content}>
-                    {...this.props.content}
+                <div id={`${styles.dropdown_content}_${this.props.name}`} className={getStyles('dropdown_content')}>
+                    {this.props.content.map(el =>
+                        el
+                    )}
                 </div>
             </div>
         );
