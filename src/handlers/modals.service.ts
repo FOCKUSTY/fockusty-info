f404dd4ef58e0753becf1bfe246f5114e889404f
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-05 21:07:17 +0500

    -style

--- src/handlers/modals.service.ts
+++ src/handlers/modals.service.ts
@@ -1,10 +1,13 @@
 class Service {
-    public Show(show: boolean = true) {
-        const modal = document.getElementById("all_modals") as HTMLElement;
+  public Show(show: boolean = true) {
+    const modal = document.getElementById("all_modals") as HTMLElement;
 
-        if (show) modal.style.display = "block";
-        else setTimeout(() => { modal.style.display = "none" }, 1000);
-    }
+    if (show) modal.style.display = "block";
+    else
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 1000);
+  }
 }
 
 export default Service;
