9115a80f72346aaec8180bf797cb5cba035b7157
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-15 23:03:51 +0500

    -hot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended, {
+    ignores: "src/styles",
     settings: {
       react: {
         version: "detect"
