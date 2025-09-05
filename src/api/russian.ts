077af03cf2337e7e4f7cf12ec0690ab790c7da20
FOCKUSTY <wwddwwwd@mail.ru>
2025-09-05 10:26:35 +0500

    update design

---
+++ src/api/russian.ts
@@ -0,0 +1,22 @@
+export const ruWords = (
+	num: number,
+	stage: [string, string, string] | [string, string]
+) => {
+	const txt: string = `${num}`,
+		firstChar: number = +txt[txt.length - 1],
+		secondChar: number = +txt[txt.length - 2];
+
+	if (num === 1 || (firstChar === 1 && secondChar != 1)) {
+    return stage[0]
+  } else if (
+		(firstChar === 1 && secondChar === 1) ||
+		firstChar === 0 ||
+		secondChar === 1
+	) {
+		return stage[2] || stage[1];
+  } else if (firstChar < 5) {
+    return stage[1]
+  } else {
+    return stage[2] || stage[1]
+  };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,132 @@
 'use client'
 
 import Link from "next/link";
-import { useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import { GROUPS, Russian } from "@/api/paths";
 import { useDropdown } from "@/components/dropdown";
 
+import { INFO, NICKNAME, DATE_OF_BIRTH } from "./page.constants";
+
 import styles from "./page.module.css";
 import { Group } from "@/components/paths";
+import { ruWords } from "@/api/russian";
+
+const dateOfBirth = new Date(DATE_OF_BIRTH.year, DATE_OF_BIRTH.month, DATE_OF_BIRTH.day, DATE_OF_BIRTH.hours, 0, 0);
+
+const SECONDS = 1000;
+
+const MINUTE = 60;
+const toMinutes = MINUTE;
+
+const HOUR  = 60;
+const toHours = toMinutes * HOUR;
+
+const DAY = 24;
+const toDay = toHours * DAY
+
+const MONTH = 31;
+
+const YEAR = 365;
+const LEAP_YEAR = 4;
+const toYear = toDay * YEAR;
+
+/* const getFullYear = (now: Date) => {
+  const days =
+    (new Time().getMonthDaysFromJanuary(now.getMonth()) + now.getDate())
+    - (time.getMonthDaysFromJanuary(DATE_OF_BIRTH.month) + dateOfBirth.getDate());
+
+  return {
+    years: now.getFullYear() - dateOfBirth.getFullYear(),
+    months: Math.floor(days / 31),
+    days: days % 31,
+    hours: now.getHours() - dateOfBirth.getHours()
+  };
+}; */
+
+const getFullYear = (now: Date) => {
+  const timestamp = (now.getTime() - dateOfBirth.getTime()) / SECONDS;
+
+  const years = Math.floor(timestamp / toYear);
+  const days = Math.floor(timestamp / toDay - (years * YEAR) - (years / LEAP_YEAR));
+  const months = Math.floor(days / MONTH);
+  const hours = Math.floor(timestamp / toHours) - (Math.floor(timestamp / toDay) * DAY);
+  const minutes = Math.floor(timestamp / toMinutes) - (Math.floor(timestamp / toHours) * HOUR);
+  const seconds = Math.floor((timestamp - (Math.floor(timestamp / toMinutes) * MINUTE)) * 10) / 10;
+
+  return {
+    years,
+    months,
+    days: days - (months * 31),
+    hours,
+    minutes,
+    seconds
+  };
+};
 
 const Page = () => {
   const [ currentGroup, setCurrentGroup ] = useState<(typeof GROUPS)[number]>("programmer");
+  const [ loaded, setLoaded ] = useState<boolean>(false);
+  const [ date, setDate ] = useState<Date>(new Date());
   const { Dropdown, setActived } = useDropdown({ id: "hobbies", className: styles.dropdown });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 100);
+
+    setLoaded(true);
+
+    return (() => {
+      clearInterval(interval);
+    });
+  }, []);
+
+  const GroupsDropdown = () => (
+    <Dropdown summary={<button>{Russian[currentGroup].toLocaleLowerCase()}</button>}>
+      {
+        GROUPS.filter(group => group !== currentGroup).map(group => (
+          <button
+            key={"btn" + group}
+            onClick={() => {
+              setCurrentGroup(group);
+              setActived(false);
+            }}
+          >{Russian[group]}</button>
+        ))
+      }
+    </Dropdown>
+  );
+
+  if (!loaded) {
+    return (
+      <div
+        className="page-center"
+        style={{
+          justifySelf: "center",
+          gap: "10px"
+        }}
+      >
+        <div className={styles.info}>
+          <h2>
+            Привет! Я {NICKNAME}, и я <GroupsDropdown />
+          </h2>
+          <div>
+            {INFO[currentGroup]}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const age = getFullYear(date);
+
+  const years = `${age.years} ${ruWords(age.years, ["год", "года", "лет"])}`;
+  const months = `${age.months} ${ruWords(age.months, ["месяц", "месяца", "месяцев"])}`;
+  const days = `${age.days} ${ruWords(age.days, ["день", "дня", "дней"])}`;
+  const hours = `${age.hours} ${ruWords(age.hours, ["час", "часа", "часов"])}`;
+  const minutes = `${age.minutes} ${ruWords(age.minutes, ["минута", "минуты", "минут"])}`;
+  const seconds = `${age.seconds} ${ruWords(age.seconds, ["секунда", "секунды", "секунд"])}`;
 
   return (
     <div
@@ -21,28 +136,18 @@ const Page = () => {
         gap: "10px"
       }}
     >
-      <Dropdown
-        summary={<button>(Выберите хобби)</button>}
-      >
-        {
-          GROUPS.filter(group => group !== currentGroup).map(group => (
-            <button
-              key={"btn" + group}
-              onClick={() => {
-                setCurrentGroup(group);
-                setActived(false);
-              }}
-            >{group}</button>
-          ))
-        }
-      </Dropdown>
+      <div className={styles.info}>
+        <h2>
+          Привет! Я {NICKNAME}, и я <GroupsDropdown />
+        </h2>
 
-      <div className={styles.group} key={currentGroup}>
-        <h2>{Russian[currentGroup]}:</h2>
-        <div className={styles.links}>
-          <Link href={"/introduction"}>Вступление</Link>
-          <Group group={currentGroup}/>
-        </div>
+        <h2>
+          Мне уже {years} {months} {days} {hours} {minutes} и {seconds}!
+        </h2>
+      </div>
+
+      <div>
+        {INFO[currentGroup]}
       </div>
     </div>
   )
