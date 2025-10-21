79dc9c8ab3e3b7c2fbf3b6d588f9897f3ccc9e6f
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-21 20:31:06 +0500

    -fix

--- src/app/me/programmer/projects/page.tsx
+++ src/app/me/programmer/projects/page.tsx
@@ -2,23 +2,61 @@
 
 import type { Project } from "@/types/project.types";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { getRepositories } from "@/services/get-repositories";
-
-import styles from "./styles.module.css";
 import { ProjectComponent } from "@/components/project.component";
 
-const sortToKey: Record<"stars" | "date" | "forks" | "name", keyof Project> = {
+import styles from "./styles.module.css";
+
+type SortingType = "stars" | "date" | "forks" | "name";
+
+const sortToKey: Record<SortingType, keyof Project> = {
   stars: "stargazers_count",
   date: "updated_at",
   forks: "forks_count",
   name: "name",
 };
 
+const russian: Record<string, string> = {
+  name: "Имя",
+  date: "Дата",
+  forks: "Форки",
+  stars: "Звезды"
+};
+
+const sort = ({
+  p1, 
+  p2,
+  sorting
+}: {
+  p1: Project,
+  p2: Project,
+  sorting: SortingType
+}) => {
+  const [v1, v2] = [p1[sortToKey[sorting]], p2[sortToKey[sorting]]];
+
+  if (v1 === null || v2 === null) {
+    return 0;
+  }
+
+  if (sorting === "name") {
+    return p1.name.localeCompare(p2.name);
+  } else if (sorting === "date") {
+    return (
+      new Date(v2.toString()).getTime() -
+      new Date(v1.toString()).getTime()
+    );
+  } else {
+    const output = +v2 - +v1;
+
+    return output === 0 ? p1.name.localeCompare(p2.name) : output;
+  }
+}
+
 const Page = () => {
   const [projects, setProjects] = useState<Project[]>([]);
-  const [sorting, setSorting] = useState<"stars" | "date" | "forks" | "name">(
+  const [sorting, setSorting] = useState<SortingType>(
     "date",
   );
 
@@ -38,51 +76,27 @@ const Page = () => {
         <div className={styles.sort}>
           <span>Сортировать по:</span>
           <div className={styles.sort_by}>
-            {sorting === "name" ? (
-              <></>
-            ) : (
-              <button onClick={() => setSorting("name")}>Имя</button>
-            )}
-            {sorting === "date" ? (
-              <></>
-            ) : (
-              <button onClick={() => setSorting("date")}>Дата</button>
-            )}
-            {sorting === "forks" ? (
-              <></>
-            ) : (
-              <button onClick={() => setSorting("forks")}>Форки</button>
-            )}
-            {sorting === "stars" ? (
-              <></>
-            ) : (
-              <button onClick={() => setSorting("stars")}>Звезды</button>
-            )}
+            {
+              Object.keys(russian).map((key) => {
+                if (sorting === key) {
+                  return null;
+                };
+
+                return (
+                  <button
+                    key={key}
+                    onClick={() => setSorting(key as SortingType)}
+                  >{russian[key]}
+                  </button>
+                );
+              })
+            }
           </div>
         </div>
 
         <div className={styles.projectsFlex}>
           {projects
-            .sort((p1, p2) => {
-              const [v1, v2] = [p1[sortToKey[sorting]], p2[sortToKey[sorting]]];
-
-              if (v1 === null || v2 === null) {
-                return 0;
-              }
-
-              if (sorting === "name") {
-                return p1.name.localeCompare(p2.name);
-              } else if (sorting === "date") {
-                return (
-                  new Date(v2.toString()).getTime() -
-                  new Date(v1.toString()).getTime()
-                );
-              } else {
-                const output = +v2 - +v1;
-
-                return output === 0 ? p1.name.localeCompare(p2.name) : output;
-              }
-            })
+            .sort((p1, p2) => sort({p1, p2, sorting}))
             .map((project) => (
               <ProjectComponent
                 key={project.id}
