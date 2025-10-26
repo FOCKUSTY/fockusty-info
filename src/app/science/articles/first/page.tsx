400afa0e72ed95c270c7718870f24aaacc3c6245
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-26 11:51:46 +0500

    -add science

---
+++ src/app/science/articles/first/page.tsx
@@ -0,0 +1,13 @@
+import Mdx from "./index.mdx";
+
+const Page = () => {
+  return (
+    <div className="page-center" style={{ alignItems: "flex-end" }}>
+      <div style={{ height: "80%", overflowY: "auto", width: "80%" }}>
+        <Mdx />
+      </div>
+    </div>
+  )
+};
+
+export default Page;
---
+++ src/app/science/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="page-center">
+      Добро пожаловать в науку
+    </div>
+  )
+};
+
+export default Page;
