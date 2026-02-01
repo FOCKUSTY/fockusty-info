b35c3b22e7bc537ca9a733e9f657c266662530c3
FOCKUSTY <wwddwwwd@mail.ru>
2026-02-01 21:24:40 +0500

    -migrate 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,32 @@
 import { Component, signal } from '@angular/core';
+import { getMyAge } from '../services/age-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly text = signal<string>("Hello, World!");
+  private readonly interval = signal<NodeJS.Timeout | null>(null);
+  
+  protected readonly currentYear = new Date().getFullYear();
+  protected readonly age = signal<string>(getMyAge(new Date()));
+
+  public constructor() {}
+
+  public ngOnInit() {
+    const interval = setInterval(() => {
+      this.age.set(getMyAge(new Date()));
+    }, 10);
+
+    this.interval.set(interval);
+  }
+
+  public ngOnDestroy() {
+    const interval = this.interval();
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    this.interval.set(null);
+  }
 }
---
+++ src/services/russian.ts
@@ -0,0 +1,25 @@
+type Stage = [string, string, string] | [string, string];
+
+export const ruWords = (num: number, stage: Stage) => {
+  const txt: string = `${num}`,
+    firstChar: number = +txt[txt.length - 1],
+    secondChar: number = +txt[txt.length - 2];
+
+  if (num === 1 || (firstChar === 1 && secondChar != 1)) {
+    return stage[0];
+  }
+
+  if (
+    (firstChar === 1 && secondChar === 1) ||
+    firstChar === 0 ||
+    secondChar === 1
+  ) {
+    return stage[2] || stage[1];
+  }
+
+  if (firstChar < 5) {
+    return stage[1];
+  }
+
+  return stage[2] || stage[1];
+};
